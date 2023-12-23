3d01afa259894902c6872d57fa1824c2e2ebe1a9
Harsh chitaliya <98301276+Harshchitaliya@users.noreply.github.com>
2023-12-23 17:34:55 +0530

    add params.yaml and modify a make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,52 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import numpy as np
+import pathlib
+from sklearn.model_selection import train_test_split
+import yaml
+import sys
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+def load_dataset(path):
+    df = pd.read_csv(path)
+    return df
+
+def data_split(data,test_split,seed):
+    X = data.drop(columns=["3"])
+    y = data["3"]
+
+    # train test split
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=test_split,random_state=seed,stratify=y)
+    return X_train, X_test, y_train, y_test
+
+def save_data(X_train, X_test, y_train, y_test,output_path):
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    X_train.to_csv(output_path + 'X_train.csv', index=False)
+    X_test.to_csv(output_path + 'X_test.csv', index=False)
+    y_train.to_csv(output_path + 'y_train.csv', index=False)
+    y_test.to_csv(output_path + 'y_test.csv', index=False)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+def main():
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    curr = pathlib.Path(__file__)
+    home_dir = curr.parent.parent.parent
+    params_file = home_dir.as_posix() + '/params.yaml'
+    params = yaml.safe_load(open(params_file))["make_dataset"]
 
-    main()
+    input_file = sys.argv[1]
+    data_path = home_dir.as_posix() + input_file
+    output_path = home_dir.as_posix() + '/data/processed/'
+
+    # Load dataset
+    df = load_dataset(data_path)
+
+    # Train test split
+    X_train, X_test, y_train, y_test = data_split(df,params["test_split"],params["seed"])
+
+    #save a data
+    save_data(X_train,X_test,y_train,y_test, output_path)
+
+if __name__ == "__main__":
+     main()
